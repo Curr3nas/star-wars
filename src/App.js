3f1e2d098ae28e4e3fa2993140455ff60f41d646
Douglas Miner <curr3nas@gmail.com>
2020-03-13 11:11:15 -0500

    first look

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 
-function App() {
+class App extends React.Component {
+
+  state = {
+    results: ''
+  }
+
+  BASE_URL = 'https://swapi.co/api/'
+
+  getData = (search) => {
+    fetch(`${this.BASE_URL}${search}`)
+    .then(res => res.json())
+    .then(data =>
+      this.setState({
+        results: data
+      }))
+  }
+
+  render() {
   return (
-    <main className='App'>
-      This is a test to make sure my deploy worked!
-    </main>
-  );
+    <body>
+      <header>
+        <h1>Star Wars Search App!</h1>
+      </header>
+      <main className='App'>
+        <p>Enter your query here! The knowledge of the Star Wars Universe awaits!</p>
+        <form className="search" onSubmit={e => this.getData(e.target.value)}>
+          <label htmlFor="search">Search</label>
+          <input type="text" id="search" name="search" placeholder="e.g. Wookie"></input>
+          <button type="submit" id="submit">Find</button>
+        </form>
+        <div className="Results">
+          <h2>Here is what our droids found</h2>
+          <ul>
+            {this.state.results.map(result => {
+              return (
+                <li>
+                  {result}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </main>
+    </body>
+  )
+  }
 }
 
 export default App;
